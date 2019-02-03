9fa97b61d2ccfa7d69e4a87137654e350dd6ed5c
Christopher J. Brody <chris.brody@gmail.com>
2019-02-02 20:35:33 -0500

    Update APP_ABI comments in jni/Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,4 @@
-#APP_ABI := all
+# Includes deprecated armeabi platform:
+# APP_ABI := all
 APP_ABI := armeabi armeabi-v7a x86 x86_64 arm64-v8a
-# For future consideration (needs testing):
-#APP_ABI += mips mips64
 
