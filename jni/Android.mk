fa4ae781017fa9d91a33364eb20035e5df444857
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-07-13 14:17:04 -0400

    refactor: rename libb64-encode -> libb64-core
    with recent updates to libb64-core

--- jni/Android.mk
+++ jni/Android.mk
@@ -11,7 +11,7 @@ LOCAL_MODULE := sqlc-native-driver
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../sqlite-amalgamation
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3-regexp-cached
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libb64-encode
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libb64-core
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3-base64
 
 LOCAL_CFLAGS += -DSQLITE_THREADSAFE=1
